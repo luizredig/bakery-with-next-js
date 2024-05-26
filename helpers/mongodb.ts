f5cce49796cc6ee43686f633fcaf5a6fc74f7964
redig <luizredig@gmail.com>
2024-05-26 14:00:39 -0300

    chore: add prisma client

---
+++ helpers/mongodb.ts
@@ -0,0 +1,9 @@
+import { prismaClient } from "@/app/lib/prisma";
+
+export const connectToMongoDB = async () => {
+  try {
+    await prismaClient.$connect();
+  } catch (error) {
+    throw new Error("Unable to connect to MongoDB database.");
+  }
+};
