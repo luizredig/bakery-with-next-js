a92b9cf27af746100671cdc105285a0d41480409
redig <luizredig@gmail.com>
2024-05-25 21:58:48 -0300

    feat: add initial header component

---
+++ app/components/header.tsx
@@ -0,0 +1,28 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card } from "./ui/card";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const Header = () => {
+  return (
+    <>
+      <Card className="flex h-20 w-full flex-row items-center justify-between rounded-none border-l-0 border-t-0 px-5 lg:px-40">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={"icon"} variant={"ghost"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side={"left"}></SheetContent>
+
+          <SheetClose asChild></SheetClose>
+        </Sheet>
+
+        <Button variant={"default"}>Login</Button>
+      </Card>
+    </>
+  );
+};
+
+export default Header;
