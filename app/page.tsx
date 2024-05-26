9b11b78444e86374b7789f69bcd3a0e20eb48c07
redig <luizredig@gmail.com>
2024-05-26 16:19:55 -0300

    feat: add initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import { connectToMongoDB } from "@/helpers/mongodb";
+import Section from "./components/section";
+import { prismaClient } from "./lib/prisma";
+
+export default async function Home() {
+  await connectToMongoDB();
+  const cakes = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "bolo",
+      },
+    },
+  });
+
   return (
-    <main></main>
+    <main>
+      <Section title="Bolos inteiros" products={cakes} />
+      <Section title="Tortas" products={cakes} />
+    </main>
   );
 }
