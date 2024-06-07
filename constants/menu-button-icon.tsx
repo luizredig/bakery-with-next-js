1a964c036fb4c86a7ef12323b7c6c5d03d591697
redig <luizredig@gmail.com>
2024-06-06 23:27:29 -0300

    feat: add menu button icon constant

---
+++ constants/menu-button-icon.tsx
@@ -0,0 +1,8 @@
+import { PlusIcon, SearchCodeIcon, SearchIcon, Trash2Icon } from "lucide-react";
+
+export const MENU_BUTTON_ICON = {
+  create: <PlusIcon size={16} />,
+  find: <SearchIcon size={16} />,
+  update: <SearchCodeIcon size={16} />,
+  delete: <Trash2Icon size={16} />,
+};
