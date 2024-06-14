1dc9b7427ae4b2abb0a1bed44b91c76ff5230658
redig <luizredig@gmail.com>
2024-06-14 18:18:45 -0300

    feat: add home page button to menu component

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,9 +1,23 @@
+import { HomeIcon } from "lucide-react";
 import MenuCollapsibleItem from "./menu-collapsible-item";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { SheetClose } from "./ui/sheet";
 
 const Menu = () => {
   return (
     <>
       <div className="flex flex-col">
+        <SheetClose asChild>
+          <Link href={"/"} className="w-full">
+            <Button className="flex w-full flex-row gap-4">
+              <HomeIcon />
+
+              <p>Home</p>
+            </Button>
+          </Link>
+        </SheetClose>
+
         <MenuCollapsibleItem slug="product" icon="cake" />
 
         <MenuCollapsibleItem slug="category" icon="clipboard" />
