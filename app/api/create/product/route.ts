bc9f1b276d18628cca0dc6313c2e65ec0c244e27
redig <luizredig@gmail.com>
2024-06-14 19:38:20 -0300

    feat: add product post endpoint

---
+++ app/api/create/product/route.ts
@@ -0,0 +1,29 @@
+import { prismaClient } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: any) {
+  const {
+    basePrice,
+    categoryId,
+    discountPercentage,
+    imageUrls = [],
+    name,
+  } = await req.json();
+
+  const lowercased = name.toLowerCase();
+
+  const slug = lowercased.replace(/\s+/g, "-");
+
+  const product = await prismaClient.product.create({
+    data: {
+      basePrice,
+      categoryId,
+      discountPercentage,
+      imageUrls,
+      name,
+      slug,
+    },
+  });
+
+  return NextResponse.json({ status: 200 });
+}
