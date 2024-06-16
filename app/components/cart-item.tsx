a6f0529eb56b4f30f9a429a794c83fa8fdae4282
redig <luizredig@gmail.com>
2024-06-16 02:22:18 -0300

    feat: enhance cart item component with new styles

--- app/components/cart-item.tsx
+++ app/components/cart-item.tsx
@@ -1,6 +1,5 @@
 import { useDispatch } from "react-redux";
 import {
-  addProductToCart,
   incrementProductQuantity,
   decrementProductQuantity,
   removeProductFromCart,
@@ -9,7 +8,7 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import { Product } from "@prisma/client";
-import { Skeleton } from "./ui/skeleton";
+import Image from "next/image";
 
 export interface ICartItem extends Product {
   quantity: number;
@@ -21,10 +20,6 @@ interface CartItemProps {
 
 const CartProduct = ({ product }: CartItemProps) => {
   const dispatch = useDispatch();
-
-  const handleAddToCart = () => {
-    dispatch(addProductToCart(product));
-  };
 
   const handleIncrementProductQuantity = () => {
     dispatch(incrementProductQuantity(product.id));
@@ -51,12 +46,26 @@ const CartProduct = ({ product }: CartItemProps) => {
         </Button>
 
         <CardContent className="flex flex-col items-center justify-between gap-4 p-5">
-          <div className="flex w-full flex-row items-center justify-start gap-4">
-            <Skeleton className="min-h-8 min-w-8 xs:h-8 xs:w-8 sm:h-16 sm:w-16  " />
+          <div className="flex w-full flex-row items-center justify-start gap-4 pt-4">
+            <Image
+              src={product.imageUrls[0]}
+              alt={product.name}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="min-h-8 min-w-8 rounded-lg xs:h-8 xs:w-8 sm:h-16 sm:w-16"
+            />
 
-            <p className="overflow-hidden text-ellipsis text-nowrap">
-              {product.name}
-            </p>
+            <div className="flex flex-col gap-1">
+              <p className="truncate font-semibold">{product.name}</p>
+
+              <p className="text-lg font-bold">
+                {Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(Number(product.basePrice * product.quantity))}
+              </p>
+            </div>
           </div>
 
           <div className="flex flex-row items-center gap-4">
