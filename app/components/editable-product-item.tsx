284c3f5c8b4cd42a89751f56ec3492068823ea74
redig <luizredig@gmail.com>
2024-06-28 00:42:54 -0300

    refactor: add discount badge and product prices to EditableProductItem component

--- app/components/editable-product-item.tsx
+++ app/components/editable-product-item.tsx
@@ -1,12 +1,13 @@
 import { Product } from "@prisma/client";
 import { Card, CardContent } from "./ui/card";
-import { PencilIcon, TrashIcon } from "lucide-react";
+import { ArrowDown, PencilIcon, PercentIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 import ImageLoader from "./image-loader";
+import { Badge } from "./ui/badge";
 
 interface IEditableProductItemProps {
   product: Product;
@@ -52,6 +53,18 @@ const EditableProductItem = ({
     <>
       <Card className="w-48 min-w-48 max-w-48 select-none overflow-hidden rounded-2xl shadow-md">
         <CardContent className="relative p-0">
+          {operation === "find" && product.discountPercentage > 0 && (
+            <Badge className="absolute left-2 top-2 z-[1] flex flex-row gap-1">
+              <ArrowDown size={16} />
+
+              <div className="flex flex-row">
+                <p>{product.discountPercentage}</p>
+
+                <PercentIcon size={16} />
+              </div>
+            </Badge>
+          )}
+
           <ImageLoader
             src={product.imageUrls[0]}
             alt={product.name}
@@ -61,6 +74,28 @@ const EditableProductItem = ({
 
           <div className="flex flex-col gap-1 px-3 py-5">
             <p className="truncate font-semibold">{product.name}</p>
+
+            {operation === "find" && (
+              <div className="flex flex-row items-center gap-1">
+                <p className="truncate text-lg font-bold">
+                  {Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(
+                    Number(
+                      product.basePrice *
+                        (1 - product.discountPercentage / 100),
+                    ),
+                  )}
+                </p>
+                <p className="truncate text-sm text-muted-foreground line-through">
+                  {Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(Number(product.basePrice))}
+                </p>
+              </div>
+            )}
 
             {operation === "update" && (
               <Button
